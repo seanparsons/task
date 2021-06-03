c5907fa885ba87e3e5f10c9f143c92c194d31403
Sean Parsons <217400+seanparsons@users.noreply.github.com>
2021-06-03 22:52:18 +0100

    Update default.nix
    Remove overriding of table-layout.

--- default.nix
+++ default.nix
@@ -1,13 +1,5 @@
 { nixpkgs ? import <nixpkgs> {} }:
 let
-  overriddenHaskellPackages = nixpkgs.pkgs.haskellPackages.override {
-    overrides = self: super: rec {
-    table-layout  = self.callCabal2nix "table-layout" (builtins.fetchGit {
-        url = "git@github.com:muesli4/table-layout.git";
-        rev = "125584715d44729952af4b9558f7ca79dbd75fca";
-      })
-      {};
-    };
-  };
+  overriddenHaskellPackages = nixpkgs.pkgs.haskellPackages;
 in
   overriddenHaskellPackages.callCabal2nix "task" (./app) {}
